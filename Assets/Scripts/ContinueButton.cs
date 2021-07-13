4f19a31b9dfa06f2326affd69d1425582aa65af2
Joao Victor <ovictorjocorvinal@gmail.com>
2021-07-12 23:26:51 -0300

    Finished application

---
+++ Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ContinueButton : MonoBehaviour
+{
+    private int sceneToContinue;
+
+    public void ContinueGame()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+
+        if(sceneToContinue != 0 )
+        {
+            SceneManager.LoadScene(sceneToContinue);
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
